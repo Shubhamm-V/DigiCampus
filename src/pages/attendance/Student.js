0d61c3ea6344d47f76b27313ee71c7af7c210ec2
Shubhamm-V <shubhamvyavhare30@gmail.com>
2023-04-17 07:45:13 +0530

    some changes

--- src/pages/attendance/Student.js
+++ src/pages/attendance/Student.js
@@ -10,9 +10,9 @@ const Student = () => {
   const [loadingScan, setLoadingScan] = useState(false);
   const [showQR, setShowQR] = useState(false);
 
-  useEffect(() => {
-    setSelected("environment");
-  }, []);
+  useEffect(()=>{
+    setSelected('environment')
+  },[])
 
   const handleScan = async (scanData) => {
     setLoadingScan(true);
@@ -56,15 +56,7 @@ const Student = () => {
                 }}
                 style={{ width: "50%" }}
               />
-              <p
-                style={{
-                  textAlign: "center",
-                  color: "greenyellow",
-                  fontWeight: "bold",
-                }}
-              >
-                {data}
-              </p>
+              <p>{data}</p>
             </Col>
           </Row>
         )}
